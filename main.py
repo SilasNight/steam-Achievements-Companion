d41c2cb99c3dd23054849a0f1080a23291dbe70f
Sias <15emiliomurray@gmail.com>
2025-10-06 11:47:25 +0200

    Placed the control block and all the buttons

--- main.py
+++ main.py
@@ -24,19 +24,37 @@ class Main:
         #Labels
         self.api_key_label = tk.Label(self.canvas_controls,text="Api Key(Sensitive)")
         self.steam_id_label = tk.Label(self.canvas_controls,text="Steam ID(Sensitive)")
+        self.achievement_load = tk.Label(self.canvas_controls,text="Load Achievements")
 
         #Buttons
         self.button_games_load = tk.Button(self.canvas_controls,text="Load Games",command=self.todo_button)
         self.button_games_previous = tk.Button(self.canvas_controls,text="Prev",command=self.todo_button)
-        self.button_games_Next = tk.Button(self.canvas_controls,text="Next",command=self.todo_button)
+        self.button_games_next = tk.Button(self.canvas_controls,text="Next",command=self.todo_button)
         self.button_games_refresh = tk.Button(self.canvas_controls,text="Refresh",command=self.todo_button)
         self.button_achievements_previous = tk.Button(self.canvas_controls,text="Prev",command=self.todo_button)
         self.button_achievements_next = tk.Button(self.canvas_controls,text="Next",command=self.todo_button)
-        self.button_achievements_ref = tk.Button(self.canvas_controls,text="Refresh",command=self.todo_button)
+        self.button_achievements_refresh = tk.Button(self.canvas_controls,text="Refresh",command=self.todo_button)
         self.button_sensitive_load = tk.Button(self.canvas_controls,text="Load Sensitive",command=self.todo_button)
         self.button_sensitive_save = tk.Button(self.canvas_controls,text="Save Sensitive",command=self.todo_button)
 
         #--Placement--
+        #control canvas
+        self.api_key_label.grid(row=0,column=0,sticky="w",columnspan=3)
+        self.api_key_entry.grid(row=1,column=0,sticky="we",columnspan=3)
+        self.steam_id_label.grid(row=2,column=0,sticky="w",columnspan=3)
+        self.steam_id_entry.grid(row=3,column=0,sticky="we",columnspan=3)
+        self.button_games_load.grid(row=4,column=0,sticky="we",columnspan=3)
+        self.button_games_previous.grid(row=5,column=0)
+        self.button_games_refresh.grid(row=5,column=1)
+        self.button_games_next.grid(row=5,column=2)
+        self.achievement_load.grid(row=6,column=0,sticky="w",columnspan=3)
+        self.button_achievements_previous.grid(row=7,column=0)
+        self.button_achievements_refresh.grid(row=7,column=1)
+        self.button_achievements_next.grid(row=7,column=2)
+        self.button_sensitive_load.grid(row=8,column=0,sticky="we",columnspan=3)
+        self.button_sensitive_save.grid(row=9,column=0,sticky="we",columnspan=3)
+
+        self.canvas_controls.grid(row=0,coloum=0,rowspan=10)
 
         self.window.mainloop()
 
